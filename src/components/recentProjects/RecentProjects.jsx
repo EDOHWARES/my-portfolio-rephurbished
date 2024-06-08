cc77b539022982ce4476be5bbde1779433b508ae
EDOHWARES <edohemmanuel4real@gmail.com>
2024-06-08 05:41:40 +0100

    edited alignments

--- src/components/recentProjects/RecentProjects.jsx
+++ src/components/recentProjects/RecentProjects.jsx
@@ -5,8 +5,8 @@ import {Projects} from '../../utils/data';
 const RecentProjects = () => {
   return (
     <section className='experience-container w-full my-[4rem] mx-auto relative'>
-        <div className='flex items-center justify-between'>
-            <h5 className='text-[1.3rem] md:text-[1.5rem] font-[600] mb-[2rem] md:mb-[3rem]'>Recent Projects</h5>
+        <div className='flex items-center justify-between text-center mb-[2rem] md:mb-[3rem]'>
+            <h5 className='text-[1.3rem] md:text-[1.5rem] font-[600]'>Recent Projects</h5>
             <a href="https://github.com/EDOHWARES" className='text-blue-800'>See more...</a>
         </div>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
--- src/components/skills/skillCard/SkillCard.jsx
+++ src/components/skills/skillCard/SkillCard.jsx
@@ -5,11 +5,11 @@ const SkillCard = ({title, icon, isActive, onClick}) => {
   return (
     <div 
         className={`skills-card ${isActive ? 'active' : ''} 
-        rounded-[0.65rem] border-[1.5px] border-[#6751b9] p-[2rem] md:p-[2.5rem] text-center bg-[#16112F65] backdrop-blur-lg relative cursor-pointer transition-all ease-out duration-500`}
+        rounded-[0.65rem] border-[1.5px] border-[#6751b9] p-[2rem] md:p-[2.5rem] text-center bg-[#16112F65] flex items-center justify-center backdrop-blur-lg relative cursor-pointer transition-all ease-out duration-500`}
         onClick={() => onClick()}
     >
 
-        <div className='skill-icon'>
+        <div className='skill-icon max-h-[5rem]'>
             <div 
             className='text-[2.4rem] text-[#6751b9] md:text-[3rem] flex items-center justify-center bg-[#2a2252] rounded-[0.65rem] border-[1.5px] border-[#6852ba] absolute top-[-1rem] left-[-1rem] p-2'>
                 {icon}
@@ -17,7 +17,7 @@ const SkillCard = ({title, icon, isActive, onClick}) => {
 
         </div>
 
-        <span className='text-[0.9rem] md:text-[1.3rem] font-[500]'>{title}</span>
+        <span className='text-[0.9rem] ml-4 md:ml-0 text-center flex items-center justify-center md:text-[1.3rem] font-[500]'>{title}</span>
         
     </div>
   )
